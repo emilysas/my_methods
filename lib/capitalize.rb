f667f80bcfd1c77fdfa532948608eb0a9b727c30
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-20 14:18:32 +0000

    adding capitalize file and tests

---
+++ lib/capitalize.rb
@@ -0,0 +1,8 @@
+class String
+
+  def my_capitalize
+
+
+  end
+
+end
---
+++ spec/capitalize_spec.rb
@@ -0,0 +1,14 @@
+describe String do
+  
+  it "can capitalize the first letter of a string" do
+    expect("hello world".capitalize).to eq("Hello world")
+  end
+
+  it "can return a string where only the first letter is capitalised" do
+    expect("HELLO WORLD".capitalize).to eq("Hello world")
+  end
+
+  it "will not have any effect on a string starting with numbers" do
+    expect("123hello".capitalize).to eq("123hello")
+  end
+end
