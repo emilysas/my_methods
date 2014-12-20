318c65ec9b0cd51d889ac97fc44f7bb8ac02099b
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-20 15:24:34 +0000

    all tests passed for my_capitalise

--- lib/capitalize.rb
+++ lib/capitalize.rb
@@ -1,8 +1,14 @@
 class String
 
   def my_capitalize
-
-
+    string = self.split(//)
+    string.each do |let|
+      if let.ord < 90
+         let = (let.ord - 32)
+      end
+    end
+    self[0] = (self[0].ord - 32).chr unless self[0] =~ /\d/
+    self
   end
 
 end
--- spec/capitalize_spec.rb
+++ spec/capitalize_spec.rb
@@ -1,7 +1,10 @@
+require 'capitalize'
+
 describe String do
   
   it "can capitalize the first letter of a string" do
     expect("hello world".capitalize).to eq("Hello world")
+    expect("hello world".my_capitalize).to eq("Hello world")
   end
 
   it "can return a string where only the first letter is capitalised" do
@@ -10,5 +13,6 @@ describe String do
 
   it "will not have any effect on a string starting with numbers" do
     expect("123hello".capitalize).to eq("123hello")
+    expect("123hello".my_capitalize).to eq("123hello")
   end
 end
