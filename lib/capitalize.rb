2b806e00dbb7611794bb2222bc81579c9fa15d1c
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-20 20:33:13 +0000

    adding changes to capitalize

--- lib/capitalize.rb
+++ lib/capitalize.rb
@@ -9,7 +9,6 @@ class String
         new_string << new_let
       else new_string << let
       end
-      
     end
     new_string[0] = (new_string[0].ord - 32).chr unless self[0] =~ /\d/
     new_string.join
