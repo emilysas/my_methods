9947fc9848c214d29e18c7908d37d9e567c299fd
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-20 20:31:12 +0000

    capitalize fixed

--- lib/capitalize.rb
+++ lib/capitalize.rb
@@ -2,13 +2,18 @@ class String
 
   def my_capitalize
     string = self.split(//)
+    new_string = []
     string.each do |let|
-      if let.ord < 90
-         let = (let.ord + 32)
+      if let =~ /[A-Z]/
+        new_let = (let.ord + 32).chr
+        new_string << new_let
+      else new_string << let
       end
+      
     end
-    self[0] = (self[0].ord - 32).chr unless self[0] =~ /\d/
-    self
+    new_string[0] = (new_string[0].ord - 32).chr unless self[0] =~ /\d/
+    new_string.join
   end
 
 end
+
